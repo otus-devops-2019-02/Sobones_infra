33210defe830b8891cd287acfff2f3fd1a3355b6
Sobones <sobones@mail.ru>
2019-04-23 15:35:30 +0300

    ADD Ansible3

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -9,3 +9,22 @@ resource "google_compute_firewall" "firewall_ssh" {
 
   source_ranges = "${var.source_ranges}"
 }
+
+resource "google_compute_firewall" "firewall_http" {
+  name = "default-allow-https"
+
+  # описание правила
+  description = "Allow HTTP connection from anywhere"
+
+  # Название сети, в которой действует правило
+  network = "default"
+
+  # Какой доступ разрешить
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  # Каким адресам разрешаем доступ
+  source_ranges = "${var.ssh_source_ranges}"
+}
